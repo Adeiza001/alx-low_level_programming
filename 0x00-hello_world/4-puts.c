630fb37e512e144fe27bf9ea5e639012750993f5
Ali Musa <alimurtadha9@gmail.com>
2022-09-12 16:13:17 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 /**
  * main - print the string in the put function
  *
- * Description:using the main function
+ * Description: using the main function
  * this program prints "Programming is like a multilingual puzzle
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
